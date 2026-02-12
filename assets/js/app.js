906faae99d00a553d9867a3d1a111c4f84b20ee5
Kharun <harun.mashanlo12@gmail.com>
2026-02-13 03:18:06 +0600

    Initial

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+const yesBtn = document.querySelectorAll(".content_btn")[0];
+const noBtn = document.querySelector(".no_btn");
+const gifImg = document.querySelector(".content_gif img");
+const text = document.querySelector(".content_text");
+const btnsWrapper = document.querySelector(".content_btns");
+
+const gifs = ["./assets/img/spang.gif", "./assets/img/cat.gif", "./assets/img/cat-2.gif"];
+
+const texts = ["Ты точно уверена? 🥺", "Подумай еще раз... 😿", "Может все же согласишься? 😭"];
+
+let currentIndex = 0;
+let btnScale = 1;
+
+noBtn.addEventListener("click", () => {
+  gifImg.style.opacity = 0;
+  text.style.opacity = 0;
+
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % gifs.length;
+
+    gifImg.src = gifs[currentIndex];
+    text.textContent = texts[currentIndex];
+
+    gifImg.style.opacity = 1;
+    text.style.opacity = 1;
+
+    btnScale -= 0.2;
+    if (btnScale < 0.2) btnScale = 0.2;
+    noBtn.style.transform = `scale(${btnScale})`;
+  }, 300);
+});
+
+yesBtn.addEventListener("click", () => {
+  gifImg.style.opacity = 0;
+  text.style.opacity = 0;
+
+  setTimeout(() => {
+    text.textContent = "Спасибоооо) Что на счет свидания?";
+    text.style.color = "#ff4d6d";
+    gifImg.src = "./assets/img/final.gif";
+
+    gifImg.style.opacity = 1;
+    text.style.opacity = 1;
+
+    btnsWrapper.style.display = "none";
+  }, 300);
+});
